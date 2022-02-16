7eac49387e259dcfad87864dee016cb45d3dc4ae
Andrew Pinner <andppinner@gmail.com>
2022-02-16 18:18:58 -0500

    Working DOCX reader

--- src/main/java/com/audioreader/AudioReader/AudioText.java
+++ src/main/java/com/audioreader/AudioReader/AudioText.java
@@ -1,54 +1,35 @@
 package com.audioreader.AudioReader;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-
-import java.io.*;
+import java.io.IOException;
+import java.util.List;
 
 public class AudioText {
+    private int counter;
 
 
     public AudioText() {
-        File file = null;
-        WordExtractor extractor = null;
-        try {
+        File file = new File("C:/Users/Student/Downloads/Document.docx");
 
-            file = new File("C:/Users/andpp/Downloads/Untitled document.docx");
-            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
-            HWPFDocument document = new HWPFDocument(fis);
-            extractor = new WordExtractor(document);
-            String[] fileData = extractor.getParagraphText();
-            for (int i = 0; i < fileData.length; i++)
-            {
-                if (fileData[i] != null)
-                    System.out.println(fileData[i]);
+        try(FileInputStream FileInputStream = new FileInputStream(file.getAbsolutePath());) {
+            XWPFDocument document = new XWPFDocument(FileInputStream);
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+            for (XWPFParagraph para : paragraphs) {
+                String paragraphText = para.getText().toLowerCase();
+                if(paragraphText.contains("right")) {
+                    counter++;
+                }
             }
-        }
-        catch (Exception e) {
+            System.out.println(counter);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-
-
-
-
-//        File testDoc = new File("C:/Users/Student/Downloads/Document.txt");
-//
-//        try(Scanner fileReader = new Scanner(testDoc)) {
-//            while(fileReader.hasNextLine()) {
-//                System.out.println("test");
-//                System.out.println(fileReader.nextLine());
-//            }
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
     }
 
 
