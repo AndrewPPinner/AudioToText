1a5159431a204fd085b1effced576a46369ee859
Andrew Pinner <andppinner@gmail.com>
2022-02-16 17:21:01 -0500

    Audio reader

---
+++ src/main/java/com/audioreader/AudioReader/AudioReaderApplication.java
@@ -0,0 +1,13 @@
+package com.audioreader.AudioReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audioreader/AudioReader/AudioReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audioreader.AudioReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
