ef7a36e543ceba6f01fd1e5c4934c17a0e587a73
Andrew Pinner <andppinner@gmail.com>
2022-03-01 11:35:36 -0500

    Added loading indicator on Admin Page

--- src/main/resources/static/adminPage/admin_page.js
+++ src/main/resources/static/adminPage/admin_page.js
@@ -2,14 +2,16 @@ const app = Vue.createApp ({
     data() {
         return {
         winningWord: null,
-        winningCount: null
+        winningCount: null,
+        isLoading: false
         }
     },
     methods: {
            getUpdate() {
+           this.isLoading = true;
                 axios
                 .get("http://localhost:7070/admin_page/update")
-                .then(res => (this.winningWord = res.data.word, this.winningCount = res.data.count))
+                .then(res => (this.winningWord = res.data.word, this.winningCount = res.data.count, this.isLoading = false))
                 .catch(e => (console.log(e)))
            }
        }
